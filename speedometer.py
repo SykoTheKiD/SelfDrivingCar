968c582cef551016dd0de82b06e0455510478957
Jay SyKo <jay@jaysyko.com>
2017-12-12 20:41:22 -0500

    Added crash detection

--- speedometer.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import pytesseract
-from PIL import ImageGrab
-
-
-screen_image = ImageGrab.grab(bbox=(0,40,800,640))
-
-print(pytesseract.image_to_string(Image.open('Capture.PNG')))
